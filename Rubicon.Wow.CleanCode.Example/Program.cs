f95cf6f682101e7d361e9f5171f696189d4679be
Jeffrey Paarhuis <jeffrey@paarhu.is>
2022-11-24 13:26:36 +0100

    remove comments

--- Rubicon.Wow.CleanCode.Example/Program.cs
+++ Rubicon.Wow.CleanCode.Example/Program.cs
@@ -1,15 +1,4 @@
-﻿//var builder = Host.CreateDefaultBuilder(args);
-
-//builder.ConfigureServices(services => 
-//        services
-//            .AddHostedService<Worker>()
-//            .AddScoped<IMessageWriter, MessageWriter>());
-
-//var host = builder.Build();
-
-//host.Run();
-
-using Rubicon.Wow.CleanCode.Example;
+﻿using Rubicon.Wow.CleanCode.Example;
 
 Console.WriteLine("Start fetching Disney characters");
 
